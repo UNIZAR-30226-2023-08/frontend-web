6cf86567366d7a76a1328630d1dca36eb479f097
Jorge Senso <819910@unizar.es>
2023-02-28 23:25:58 +0100

    Ajusta titulo sala espera

--- src/views/WaitingRoomPage/WaitingRoomPage.js
+++ src/views/WaitingRoomPage/WaitingRoomPage.js
@@ -7,7 +7,7 @@ export function WaitingRoomPage({ roomSize=4 }) {
 
   return (
     <div className="grid grid-rows-[1fr_9fr] h-screen">
-      <p className="row-start-1 flex justify-center items-end sm:my-[3%] font-semibold lg:text-4xl sm:text-3xl text-neutral-600 dark:text-neutral-200">Esperando jugadores</p>
+      <p className="row-start-1 flex justify-center items-end sm:my-[3%] lg:mb-[-3vh] font-semibold lg:text-4xl sm:text-3xl text-neutral-600 dark:text-neutral-200">Esperando jugadores</p>
       <div className="row-start-2 flex w-screen h-fit lg:h-[80%] sm:mt-2 justify-evenly items-center shrink-0">
         {players.map((player, i) => (
           <PlayerCard key={i} name={player} />
