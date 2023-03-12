ed922ef0383fa994bde5a611d2ffbae3b6713755
Jorge Senso <819910@unizar.es>
2023-03-12 19:46:15 +0100

    Implementa todo el css de Game.js con tailwind

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,6 +1,5 @@
 import { useContext, useState } from "react";
 import { ThemeContext } from "../../context/ThemeContext";
-import "./Game.css";
 
 export function HandCard({ value, selected, onSelect }) {
   const theme = useContext(ThemeContext)
@@ -38,6 +37,7 @@ export function Hand({ myTurn=false }) {
           }, 400)
         }
         className={`ml-5 bg-primary-500 text-neutral-100 font-bold py-2 px-4 rounded-full ${!(selected && myTurn) && 'opacity-50 cursor-not-allowed  hover:bg-primary-700'}`}
+        disabled={!(selected && myTurn)}
         >
         Jugar
       </button>
@@ -46,26 +46,37 @@ export function Hand({ myTurn=false }) {
 }
 
 export function Played({ playedCards }) {
-  let classname = "played-card played-card-";
-  const img = Object.keys(playedCards).map((key, index) => (
-    <div key={key} className={classname + key}>
+  function placeCard(key){
+    switch(key) {
+      case "j1": return "row-start-3 row-end-3 col-start-2 col-end-2"
+      case "j2": return "row-start-2 row-end-2 col-start-3 col-end-3 -rotate-90"
+      case "j3": return "row-start-1 row-end-1 col-start-2 col-end-2"
+      case "j4": return "row-start-2 row-end-2 col-start-1 col-end-1 rotate-90"
+    } 
+  }
+
+  const theme = useContext(ThemeContext)
+  const img = Object.keys(playedCards).map((key) => (
+    <div key={key} className={`flex justify-center items-center ${placeCard(key)}`}>
       <img
-        // className={classname + key}
-        src={"https://via.placeholder.com/120x150?text=" + playedCards[key]}
+        className="sm:max-h-[200px] lg:max-h-[220px]"
+        src={theme[playedCards[key]]}
       ></img>
     </div>
   ));
 
-  return <div className="played-container">{img}</div>; //TODO refactor
+  return <div className="grid grid-rows-3 grid-cols-3 col-start-2 col-end-2 row-start-2">{img}</div>; //TODO refactor
 }
 
 export function Deck({ triunfo }) {
+  const theme = useContext(ThemeContext)
   return (
-    <div className="deck-container">
-      <img src={"https://via.placeholder.com/120x150/ffdfba?text=Deck"}></img>
+    <div className="flex justify-center items-center xs:row-start-1 lg:row-start-2">
+      <img className="sm:max-h-[150px] lg:max-h-[200px]"
+      src={theme.dorso}></img>
       <img
-        className="triunfo"
-        src={"https://via.placeholder.com/120x150/ffb3ba?text=" + triunfo}
+        className="sm:max-h-[150px] lg:max-h-[200px] rotate-90 overflow-hidden ml-[-3em] -z-10"
+        src={theme[triunfo]}
       ></img>
     </div>
   );
