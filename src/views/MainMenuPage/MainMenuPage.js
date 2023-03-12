f578f9978e2994b45dd48909ec000a79894aaf5b
Jorge Senso <819910@unizar.es>
2023-03-12 19:41:35 +0100

    Primera version menu principal

---
+++ src/views/MainMenuPage/MainMenuPage.js
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom";
+
+export function MainMenuPage() {
+  return (
+    <div className="flex h-[80vh] flex-col justify-center items-center">
+      <img
+        className="mb-4"
+        src="https://via.placeholder.com/150 "
+        alt="placeholder"
+      />
+      <div className="flex flex-col max-w-lg md:min-w-[50vw] sm:min-w-[80vw]">
+        <button
+          className="mt-4 w-full rounded bg-primary-500 px-6 py-2.5 text-md font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-primary-700 hover:shadow-lg focus:bg-primary-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-800 active:shadow-lg"
+          data-te-ripple-init
+          data-te-ripple-color="light"
+        >
+          Entrada 1
+        </button>
+        <button
+          className="mt-4 w-full rounded bg-primary-500 px-6 py-2.5 text-md font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-primary-700 hover:shadow-lg focus:bg-primary-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-800 active:shadow-lg"
+          data-te-ripple-init
+          data-te-ripple-color="light"
+        >
+          Entrada 2
+        </button>
+        <button
+          className="mt-4 w-full rounded bg-primary-500 px-6 py-2.5 text-md font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-primary-700 hover:shadow-lg focus:bg-primary-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-800 active:shadow-lg"
+          data-te-ripple-init
+          data-te-ripple-color="light"
+        >
+          Entrada 3
+        </button>
+      </div>
+    </div>
+  );
+}
