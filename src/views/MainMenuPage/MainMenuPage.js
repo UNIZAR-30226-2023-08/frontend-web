5dd19fc3ce1782548700c075412eecd5fd5f57db
Jorge Senso <819910@unizar.es>
2023-05-10 23:05:10 +0200

    Modifica main menu

--- src/views/MainMenuPage/MainMenuPage.js
+++ src/views/MainMenuPage/MainMenuPage.js
@@ -15,27 +15,29 @@ export function MainMenuPage() {
             data-te-ripple-init
             data-te-ripple-color="light"
           >
-            4 jugadores
+            Jugar
           </button>
         </Link>
-        <Link to="/search">
+        <Link to="/">
           <button
             className="mt-4 w-full rounded bg-primary-500 px-6 py-2.5 text-md font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-primary-700 hover:shadow-lg focus:bg-primary-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-800 active:shadow-lg"
             data-te-ripple-init
             data-te-ripple-color="light"
           >
-            2 jugadores
+            Estadisticas
           </button>
         </Link>
-        <Link to="/game">
-          <button
-            className="mt-4 w-full rounded bg-primary-500 px-6 py-2.5 text-md font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-primary-700 hover:shadow-lg focus:bg-primary-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-800 active:shadow-lg"
-            data-te-ripple-init
-            data-te-ripple-color="light"
-          >
-          Entrenamiento vs Ia
-          </button>
-        </Link>
+        <button
+          className="mt-4 w-full rounded bg-primary-500 px-6 py-2.5 text-md font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-primary-700 hover:shadow-lg focus:bg-primary-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-800 active:shadow-lg"
+          data-te-ripple-init
+          data-te-ripple-color="light"
+          onClick={() => {
+            localStorage.removeItem('access_token')}
+            
+          }
+        >
+          Cerrar sesión
+        </button>
       </div>
     </div>
   );
