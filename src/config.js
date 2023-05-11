5503adf5866c084718edbb6e1a463d1836c96580
Jorge Senso <819910@unizar.es>
2023-05-11 12:28:36 +0200

    Deploy React app to GitHub Pages

--- src/config.js
+++
@@ -1,4 +0,0 @@
-export const BACKEND_URL = "localhost:8000";
-export const CHAT_ENDPOINT = "/ws/";
-export const REGISTER_ENDPOINT = "/register";
-export const LOGIN_ENDPOINT = "/token";
--- src/views/GamePage/GamePage.js
+++
@@ -1,7 +0,0 @@
-import { useEffect, useState } from "react";
-import { useLocation } from "react-router";
-import { Game } from "../../components/Game/Game";
-
-export function GamePage({url, newGame, numJugadores}) {
-  return <Game newGame={newGame} serverUrl={url} numJugadores={numJugadores}/>
-}
